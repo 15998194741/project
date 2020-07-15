7b59c74bbdabdd6a5d63068ac79c6b7c638b5c22
lixindong <lixindong@fancyguo.cn>
2020-07-15 20:28:09 +0800

    test

--- view/vue.config.js
+++ view/vue.config.js
@@ -38,9 +38,11 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:10010',
+        // target: 'http://127.0.0.1:10010',
         // target: 'http://10.1.35.78:10010',
         // target: 'http://123.57.86.38:10010',
+        // target: 'http://127.0.0.1:10010',
+        target: 'http:106.75.7.83',
         changeOrigin: true
       }
     }
