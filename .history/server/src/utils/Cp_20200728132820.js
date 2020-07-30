a5b090b95fe593e783761e70b9253bdd1cce93ce
lixindong <lixindong@fancyguo.cn>
2020-07-30 20:54:23 +0800

    111

---
+++ .history/server/src/utils/Cp_20200728132820.js
@@ -0,0 +1,46 @@
+
+const request = require('request');
+
+class Cp{
+	constructor(){	
+		this.url= 'http://localhost:30000/api/';
+	}
+	async get(url, query){
+		let res = {
+			url:this.url+url,
+			method:'GET',
+			query
+		};
+		return new Promise((resolve, reject)=>{
+			request(res, (error, response, body)=>{
+				if(!error && response.statusCode == 200){
+					return resolve(body);
+				}else{
+					return resolve(error);			
+				}
+			});
+
+		}); 
+	}
+	async put(url, data){
+		let res = {
+			url:this.url+url,
+			method:'PUT',
+			data
+		};
+	
+		return new Promise((resolve, reject)=>{
+			request(res, (error, response, body)=>{
+				if(!error && response.statusCode == 200){
+					return resolve(body);
+				}else{
+					return resolve(error);			
+				}
+			});
+
+		}); 
+	}
+
+}
+
+export default new Cp();
